446d0c9f9e756f2a5e8e31f4a9baee8fd6706a98
cigetbudi <cigetbudi@gmail.com>
2022-06-19 11:59:20 +0700

    first commit

---
+++ models/personals.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Personals struct {
+	Id        int
+	Nama      string
+	Alamat    string
+	Phone     string
+	Umur      int
+	Pekerjaan int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}

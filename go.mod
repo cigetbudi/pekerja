5af7afc0bc392782a4f38207d1e7e368d1f6058f
cigetbudi <cigetbudi@gmail.com>
2022-06-26 00:28:37 +0700

    Securing Routes with middleware

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -25,6 +26,7 @@ require (
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/gorm v1.23.6 // indirect
 )
--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"pekerja/controllers"
+	"pekerja/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -11,9 +12,9 @@ func Init() *echo.Echo {
 
 	route.GET("/", controllers.FetchAllPersonal)
 	route.GET("personal/cari", controllers.FetchAllPersonal)
-	route.POST("personal/tambah", controllers.AddPersonal)
-	route.PUT("personal/edit/:email", controllers.EditPersonal)
-	route.DELETE("personal/hapus/:email", controllers.HapusPersonal)
+	route.POST("personal/tambah", controllers.AddPersonal, middleware.IsAuthenticated)
+	route.PUT("personal/edit/:email", controllers.EditPersonal, middleware.IsAuthenticated)
+	route.DELETE("personal/hapus/:email", controllers.HapusPersonal, middleware.IsAuthenticated)
 
 	route.GET("/genhash/:password", controllers.GenerateHashPass)
 	route.POST("/login", controllers.CheckLogin)

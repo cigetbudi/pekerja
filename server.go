5e571cc3f84483d6da7d1c9fa5716bc935221358
cigetbudi <cigetbudi@gmail.com>
2022-06-26 00:29:52 +0700

    Change Environtment

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"pekerja/db"
 	"pekerja/routes"
 )
@@ -9,6 +10,8 @@ func main() {
 	db.Init()
 	route := routes.Init()
 
-	route.Start(":3435")
+	// route.Start(":3435")
+	port := os.Getenv("PORT")
+	route.Logger.Fatal(route.Start(":" + port))
 
 }

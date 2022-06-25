6f251c1dc92e7bcc3aa0ccccbff86de813403100
cigetbudi <cigetbudi@gmail.com>
2022-06-25 20:34:50 +0700

    Changing Remote Web API Port

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"pekerja/db"
 	"pekerja/routes"
 )
@@ -8,7 +9,7 @@ import (
 func main() {
 	db.Init()
 	route := routes.Init()
-
-	route.Start(":3435")
+	port := os.Getenv("PORT")
+	route.Logger.Fatal(route.Start(":" + port))
 
 }
